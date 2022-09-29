0873435f3f7b0909463f93f36eb29a569f687dc4
minhngth2409 <minhngth2409@gmail.com>
2022-09-30 02:00:02 +0700

    first commit

--- app/index.js
+++ app/index.js
@@ -26,67 +26,30 @@ typingText(["Thanh Minh.", "A Student.", "A Web Developer."]);
 const sections = $$(".section");
 const panes = $$(".pane");
 
-// const tl = gsap.timeline({
-//   defaults: { duration: 0.5, opacity: 0 },
-// });
-
-// tl.from(".left", {
-//   height: "0",
-// })
-//   .from(".avatar", {
-//     height: "0",
-//   })
-//   .from(".circle-1", {
-//     height: "0",
-//   }, 0)
-//   .from(".circle-2", {
-//     height: "0",
-//   }, 0)
-//   .from(".name", {
-//     height: "0",
-//   })
-//   .from(".description", {
-//     height: "0",
-//   }, '<')
-//   .from(".section", {
-//     height: "0",
-//   });
-
-// tl.from('.right', {
-//   height: "0",
-// }, )
-// .from('.title', {
-//   height: "0",
-// }, '<')
-
 sections.forEach((item, index) => {
   const pane = panes[index];
-  item.onclick = function() {
+  item.onclick = function () {
     $(".section.active").classList.remove("active");
     $(".pane.active").classList.remove("active");
 
     pane.classList.add("active");
     item.classList.add("active");
-
-    const tl = new TimelineLite();
-    tl.from(pane, 0.5, {
-      x: '-30px',
-      opacity: 0
-    })
-    // .from('.skill-item', {
-    //   x:'30px',
-    //   // y:'50px',
-    //   opacity: 0,
-    //   width: '0',
-    // }, 0)
-    .from('.showcase', {
-      x: '30px',
-      y: '50px',
-    }, 0)
-    .from('.skill-icon', {
-      rotate: '180deg',
-      scale: 0,
-    }, 0)
-
   };
 });
+
+const tl = gsap.timeline();
+
+tl.from(".bg_text", {
+  opacity: 0,
+  y: "-100px",
+  duration: 1,
+})
+  .from("#typing-text", {
+    opacity: 0,
+    y: "-100px",
+  })
+  .from(".cubic_side", {
+    opacity: 0,
+    scale: 0,
+    duration: 1,
+  });
